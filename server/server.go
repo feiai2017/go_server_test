9f9bcac47b086382d2d042d68865340a3dc4fb21
wangpf <oncwnuNiTDOl00Q4ak5mLqaYLgaU@git.weixin.qq.com>
2020-10-13 15:13:00 +0800

    ok3

--- server/server.go
+++ server/server.go
@@ -12,24 +12,22 @@ type PlayerStore interface {
 
 type PlayerServer struct {
 	store PlayerStore
-	router *http.ServeMux
+	http.Handler
 }
 
 func NewPlayerServer(store PlayerStore) *PlayerServer {
-	p := &PlayerServer{
-		store,
-		http.NewServeMux(),
-	}
+	p := new(PlayerServer)
 
-	p.router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	p.store = store
+	router := http.NewServeMux()
 
-	p.router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+
+	p.Handler = router
 
 	return p
-}
-
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
