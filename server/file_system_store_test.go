ae4343a99d06bf997ccd12097d6e01c1a8cb1362
wangpf <oncwnuNiTDOl00Q4ak5mLqaYLgaU@git.weixin.qq.com>
2020-10-14 11:42:47 +0800

    ok 1

--- server/file_system_store_test.go
+++ server/file_system_store_test.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"encoding/json"
 	"io"
 	"strings"
 	"testing"
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
-	var league []Player
-	json.NewDecoder(f.database).Decode(&league)
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
 	return league
 }
 
-func TestFileSystemStore(t *testing.T)  {
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
+}
+
+func TestFileSystemStore(t *testing.T) {
 
 	t.Run("/league from a reader", func(t *testing.T) {
 		database := strings.NewReader(`[
@@ -34,5 +45,28 @@ func TestFileSystemStore(t *testing.T)  {
 		}
 
 		assertLeague(t, got, want)
+
+		got = store.GetLeague()
+		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]
+		`)
+
+		store := FileSystemPlayerStore{database}
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+
+		assertScoreEquals(t, got, want)
+	})
+}
+
+func assertScoreEquals(t *testing.T, got, want int)  {
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
