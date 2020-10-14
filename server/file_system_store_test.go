77494c3b42cd5c02b4c30845a4a65b8853f6e3b6
wangpf <oncwnuNiTDOl00Q4ak5mLqaYLgaU@git.weixin.qq.com>
2020-10-14 14:54:40 +0800

    ok 3

--- server/file_system_store_test.go
+++ server/file_system_store_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"os"
@@ -11,22 +12,33 @@ type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
 }
 
-func (f *FileSystemPlayerStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() League {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+
+	player := f.GetLeague().Find(name)
+
+	if player != nil {
+		return player.Wins
 	}
 
-	return wins
+	return 0
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+	player := league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	}
+
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
 }
 
 func TestFileSystemStore(t *testing.T) {
@@ -55,8 +67,7 @@ func TestFileSystemStore(t *testing.T) {
 	t.Run("get player score", func(t *testing.T) {
 		database, cleanDatabase := createTempFile(t,`[
 			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]
-		`)
+			{"Name": "Chris", "Wins": 33}]`)
 		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
@@ -64,6 +75,21 @@ func TestFileSystemStore(t *testing.T) {
 
 		want := 33
 
+		assertScoreEquals(t, got, want)
+	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t,`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Chris")
+
+		got := store.GetPlayerScore("Chris")
+		want := 34
 		assertScoreEquals(t, got, want)
 	})
 }
